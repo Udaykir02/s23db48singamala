6c556dbc23eae73855e607cf313e255739deb96d
Uday Singamala <143046094+Udaykir02@users.noreply.github.com>
2023-11-09 18:30:20 -0600

    put and get with id

--- controllers/horses.js
+++ controllers/horses.js
@@ -12,15 +12,17 @@ exports.horses_list = async function (req, res) {
 };
 
 // Get details for a specific Horse.
-exports.horses_detail = async function (req, res) {
+exports.horses_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
     try {
-        const detail = await horses.findById(req.params.id);
-        console.log("Fetched the horses details " + detail);
-        res.send(detail);
+    result = await horses.findById( req.params.id)
+    res.send(result)
     } catch (error) {
-        res.status(500).send(`{"error": "Document for id ${req.params.id} not found"}`);
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
     }
-};
+    };
+    
 
 
 // Handle Horse create on POST.
@@ -52,21 +54,22 @@ exports.horses_delete = async function (req, res) {
 };
 
 // Handle Horse update form on PUT.
-exports.horses_update_put = async function (req, res) {
+exports.horses_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
     try {
-        let horsesUpdate = await horses.findById(req.params.id);
-        if (req.body.horse_name)
-            horsesUpdate.horse_name = req.body.horse_name;
-        if (req.body.horse_age)
-            horsesUpdate.horse_age = req.body.horse_age;
-        if (req.body.horse_price)
-            horsesUpdate.horse_price = req.body.horse_price;
-        let result = await horsesUpdate.save();
-        console.log("Successfully Updated the Horse " + result);
-        res.send(result);
+        let toUpdate = await horses.findById(req.params.id)
+        // Do updates of properties
+        if(req.body.horse_name)
+            toUpdate.horse_name = req.body.horse_name;
+        if(req.body.horse_age) toUpdate.horse_age = req.body.horse_age;
+        if(req.body.horse_price) toUpdate.horse_price = req.body.horse_price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
     } catch (err) {
-        res.status(500);
-        res.send(`{"error": "${err}: Update for id ${req.params.id} failed"}`);
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+        failed`);
     }
 };
 
