1aca91cbd1eb47c23f2bc309307d576e1e0957fb
Uday Singamala <143046094+Udaykir02@users.noreply.github.com>
2023-11-26 14:47:56 -0600

    login added

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-const Horse = require('./models/horses'); // Check the path to the model file
+const Horse = require('./models/horses');
+const Account = require('./models/account');
 
-
-
-const connectionString ="mongodb+srv://s564148:0rzNLg5AMhLLIyaJ@cluster0.xoykge7.mongodb.net/?retryWrites=true&w=majority" || process.env.MONGO_CONN;
+const connectionString = "mongodb+srv://s564148:0rzNLg5AMhLLIyaJ@cluster0.xoykge7.mongodb.net/?retryWrites=true&w=majority" || process.env.MONGO_CONN;
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -24,20 +21,54 @@ db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 db.once("open", function () {
   console.log("Connection to DB succeeded");
 });
-// passport config
-// Use the existing connection
-// The Account model
-var Account =require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+
+        // Use the authenticate method provided by passport-local-mongoose
+        user.authenticate(password, function(err, isMatch) {
+          if (err) {
+            return done(err);
+          }
+
+          if (!isMatch) {
+            return done(null, false, { message: 'Incorrect password.' });
+          }
+
+          return done(null, user);
+        });
+      })
+      .catch(function (err) {
+        return done(err);
+      });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  Account.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => {
+      done(err);
+    });
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
 var resourceRouter = require('./routes/resource');
-var horsesRouter = require('./routes/horses'); // Import the horses router
+var horsesRouter = require('./routes/horses');
 
 var app = express();
 
@@ -49,38 +80,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-  Account.findOne({ username: username })
-  .then(function (user){
-  if (err) { return done(err); }
-  if (!user) {
-  return done(null, false, { message: 'Incorrect username.' });
-  }
-  if (!user.validPassword(password)) {
-  return done(null, false, { message: 'Incorrect password.' });
-  }
-  return done(null, user);
-  })
-  .catch(function(err){
-  return done(err)
-  })
-  })
-  )
-  app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/board', boardRouter);
 app.use('/selector', chooseRouter);
 app.use('/resource', resourceRouter);
-app.use('/horses', horsesRouter); // Use the horses router for /horses routes
+app.use('/horses', horsesRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));
@@ -94,7 +109,6 @@ app.use(function (err, req, res, next) {
 });
 
 async function recreateDB() {
-  // Delete everything from the Horse model
   await Horse.deleteMany();
 
   let instance1 = new Horse({ horse_name: 'Midnight Shadow', horse_age: 6, horse_price: 5500 });
